6f44db7eac439833d2dca06d393d2f890c4ff255
pupa <machuchalov@stud.kpfu.ru>
2024-05-01 16:05:25 +0400

    added homework5

---
+++ homework5/src/main/java/ru/vtb/t1/course/config/DataSourceConfiguration.java
@@ -0,0 +1,19 @@
+package ru.vtb.t1.course.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Configuration
+@EnableConfigurationProperties(value = HikariProperties.class)
+public class DataSourceConfiguration {
+
+    @Bean
+    public Statement getStatement(HikariProperties hikariProperties) throws SQLException {
+        return new HikariDataSource(hikariProperties).getConnection().createStatement();
+    }
+}
---
+++ homework5/src/main/java/ru/vtb/t1/course/repository/UserRepository.java
@@ -0,0 +1,67 @@
+package ru.vtb.t1.course.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.vtb.t1.course.model.UserDTO;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private final Statement statement;
+
+    public UserRepository(Statement statement) {
+        this.statement = statement;
+    }
+
+    public void save(UserDTO userDTO) {
+        var createQuery = String.format("INSERT INTO users(id, username) VALUES (%s, '%s')", userDTO.getId(), userDTO.getUsername());
+        execute(createQuery);
+    }
+
+    public UserDTO findById(Long userId) {
+        var findByIdQuery = String.format("SELECT * FROM users WHERE id = %s", userId);
+        var users = executeWithResult(findByIdQuery);
+        if (users.isEmpty()) {
+            return null;
+        } else if (users.size() == 1) {
+            return users.get(0);
+        }
+        throw new RuntimeException("found not unique users");
+    }
+
+    public List<UserDTO> findAll() {
+        var findAllQuery = "SELECT * FROM users";
+        return executeWithResult(findAllQuery);
+    }
+
+    public void delete(Long userId) {
+        var deleteQuery = String.format("DELETE FROM users WHERE id = %s", userId);
+        execute(deleteQuery);
+    }
+
+    private void execute(String sql) {
+        try {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<UserDTO> executeWithResult(String sql) {
+        List<UserDTO> users = new ArrayList<>();
+        try {
+            var result = statement.executeQuery(sql);
+            while (result.next()) {
+                var user = new UserDTO(result.getLong("id"), result.getString("username"));
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
